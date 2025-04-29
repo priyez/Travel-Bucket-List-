fdf835c6f09037d23738112039cc6a9f8baf390a
priyez <jayspriye@gmail.com>
2025-04-29 19:45:39 +0100

    second commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,23 +1,18 @@
 'use client';
 import MapView from '@/components/MapView';
 import DestinationList from '@/components/DestinationList';
-import { useState } from 'react';
+
 
 export default function Dashboard() {
-    const [coordinates, setCoordinates] = useState<[number, number] | null>(null);
 
 
     return (
         <main className="h-screen">
-            <MapView  />
+            <MapView />
             <div className="p-3  fixed z-10 top-1">
                 <DestinationList />
             </div>
-            {coordinates && (
-                <div className="absolute bottom-4 left-4 bg-white/80 text-black px-3 py-1 rounded">
-                    Lat: {coordinates[0].toFixed(4)}, Lng: {coordinates[1].toFixed(4)}
-                </div>
-            )}
+
 
         </main>
     );
